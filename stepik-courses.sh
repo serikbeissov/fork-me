a7e206c6269a5ba1b2dbff20efd6d7e3b0fbd537
serikbeissov <beissov.s@gmail.com>
2024-10-02 23:40:45 +0500

    fix script

--- stepik-courses.sh
+++ stepik-courses.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-curl -s https://stepik.org:443/api/course-lists\?page\=1 | jq
+curl -s https://stepik.org:443/api/course-lists\?page\=1 | jq --raw-output '."course-lists"[].title'
